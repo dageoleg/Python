daacee33ebc9da18da7f682a83048fab47274090
Aleksandr Segeev <sandy.sergeev@mail.ru>
2020-02-05 21:53:13 +0300

    cheeseburger

---
+++ Practice/a.sergeev/cf1.py
@@ -0,0 +1,14 @@
+def compare(a, b):
+    if a>=b:
+        print (str(a) + " is bigger")
+    else:
+        print (str(b) + " is bigger")
+
+print ('vvedi  ssuka chislo dlya x')
+x = input()
+
+print ('vvedi  ssuka chislo dlya y')
+y = input()
+
+compare (x, y)
+    
