1f7d5d4c024f18db0fbb1d1758ddcb077d5fb71e
AlexanderIsaev1995 <Alexander.095.Isaev@gmail.com>
2020-02-05 21:33:22 +0300

    Add my new file

---
+++ Practice/a.isaev/a.isaev_compare.py
@@ -0,0 +1,9 @@
+def compare(a,b):
+    if a>b:
+        print(f"Первое число больше \nОно равно {a}")
+    if a==b:
+        print(f'Числа равны')
+    if b>a:
+        print(f"Второе число больше \nОно равно {b}")
+
+compare(200,150)
